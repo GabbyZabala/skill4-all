3d2194c04b2c67287f832937f2e9b932e44a48ea
MacCloud <cameronmaccloud13@gmail.com>
2024-02-21 13:30:06 +0800

    Beta 0.5

--- data/index.js
+++ data/index.js
@@ -2,7 +2,8 @@ const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 const logo = document.querySelector('.logo');
 logo.addEventListener('click', returnToHome);
-
+var bg = document.getElementById("body");
+    bg.className = "home";
     function handleTabClick(tab) {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
@@ -22,7 +23,6 @@ logo.addEventListener('click', returnToHome);
     function returnToHome() {
         const homeTab = document.querySelector('[data-tab-target="#home"]');
         const homeSection = document.querySelector('#home');
-
         tabs.forEach(tab => {
             tab.classList.remove('active');
         });
@@ -31,4 +31,26 @@ logo.addEventListener('click', returnToHome);
         });
         homeTab.classList.add('active');
         homeSection.classList.add('active');
+        bg.className = "";
+        bg.classList.add("home");
+    }
+    function toggleDropdown() {
+        var dropdown = document.getElementById("dropdown");
+        var accountLine = document.getElementById("account-line");
+        var profilePic = document.getElementById("profile-pic");
+        dropdown.classList.toggle("showshit");
+        accountLine.classList.toggle("bline");
+        profilePic.classList.toggle("blank");
+    }
+    function bghome() {
+        bg.className = "";
+        bg.classList.add("home");
+    }
+    function bgabout() {
+        bg.className = "";
+        bg.classList.add('about');
+    }
+    function bgview() {
+        bg.className = "";
+        bg.classList.add('view');
     }
